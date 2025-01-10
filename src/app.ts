9407959b38344e1c0d1c8a20051a9e5528fdfcc5
M Rifki Septiawan <rifkim91@gmail.com>
2025-01-10 20:54:14 +0700

    feat: add routes for regional song model

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,14 @@ import { Hono } from 'hono'
 import provinceRoutes from './routes/provinceRoutes'
 import ethnicgroupRoutes from './routes/ethnicgrouproutes'
 import cultureRoutes from './routes/cultureroutes'
+import regionalsongRoutes from './routes/regionalsongroutes'
 
 const app = new Hono()
 
 app.route('/provinces', provinceRoutes)
 app.route('/ethnics', ethnicgroupRoutes)
-app.route('/culture', cultureRoutes)
+app.route('/cultures', cultureRoutes)
+app.route('/songs', regionalsongRoutes)
 
 export default app
 
