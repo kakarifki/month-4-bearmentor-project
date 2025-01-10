74e855a621de8b94e237d2bdc705abde7bc2abc3
M Rifki Septiawan <rifkim91@gmail.com>
2025-01-10 21:38:59 +0700

    initial openApi and Swagger UI

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,19 @@
-import { Hono } from 'hono'
+import { OpenAPIHono, createRoute, z } from '@hono/zod-openapi';
+import { swaggerUI } from '@hono/swagger-ui';
 import provinceRoutes from './routes/provinceRoutes'
 import ethnicgroupRoutes from './routes/ethnicgrouproutes'
 import cultureRoutes from './routes/cultureroutes'
 import regionalsongRoutes from './routes/regionalsongroutes'
 import cuisineRoutes from './routes/cuisineroutes'
 
-const app = new Hono()
+const app = new OpenAPIHono()
+
+app.get(
+    '/ui',
+    swaggerUI({
+        url: '/doc', // Path untuk OpenAPI JSON
+    })
+);
 
 app.route('/provinces', provinceRoutes)
 app.route('/ethnics', ethnicgroupRoutes)
