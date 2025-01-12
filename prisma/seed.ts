ac3338e09c56ba2643272019b6d2c34a1500c685
M Rifki Septiawan <rifkim91@gmail.com>
2025-01-13 06:33:06 +0700

    feat: package.json sorted for dependecies and devdependencies

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,9 @@
-import { PrismaClient } from "@prisma/client";
-import provincesData from "../src/seeding/outputProvince.json"
-import ethnicGroupData from "../src/seeding/outputEthnic.json"
+import { PrismaClient, CultureCategory } from "@prisma/client";
+import provincesData from "../src/seeding/outputProvince.json";
+import ethnicGroupData from "../src/seeding/outputEthnic.json";
+import culturesData from "../src/seeding/outputCulture.json";
+import regionalSongsData from "../src/seeding/outputSong.json";
+import cuisinesData from "../src/seeding/outputCuisine.json";
 
 const prisma = new PrismaClient()
 
@@ -11,11 +14,35 @@ async function main() {
     })
     console.log('Seeding Provinces completed.')
 
-    //
+    // Seeding Ethnic Group
     await prisma.ethnicGroup.createMany({
         data: ethnicGroupData,
     })
-    console.log('Seeding Ethnic Code completed.')
+    console.log('Seeding Ethnic Groups completed.')
+
+    // Format Category menjadi enum
+    const formattedCulturesData = culturesData.map((culture) => ({
+        ...culture,
+        category: CultureCategory[culture.category as keyof typeof CultureCategory],
+    }))
+
+    // Seeding Culture
+    await prisma.culture.createMany({
+        data: formattedCulturesData,
+    })
+    console.log('Seeding Cultures completed.')
+
+    // Seeding Regional Song
+    await prisma.regionalSong.createMany({
+        data: regionalSongsData,
+    })
+    console.log('Seeding Regional Songs completed.')
+
+    // Seeding Cuisine
+    await prisma.cuisine.createMany({
+        data: cuisinesData,
+    })
+    console.log('Seeding Cuisines completed.')
 }
 
 main()
